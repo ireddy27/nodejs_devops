170b476b4bfdebd2d11082e8b21c7540f4ef0314
Isha6927 <98211228+ireddy27@users.noreply.github.com>
2024-11-08 14:07:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World</h1>'); // Send HTML response with "Hello World"
+});
 
 app.get('/status', (req, res) => {
     res.json({ status: 'API is running' });
